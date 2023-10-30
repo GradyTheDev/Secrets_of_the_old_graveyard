2f30c292eef7603294a2dbf4812ba973f2eba39e
GradyTheDev <50427197+GradyTheDev@users.noreply.github.com>
2023-10-30 16:34:15 -0500

    sound editse

--- code/code_resources/button_sound.gd
+++ code/code_resources/button_sound.gd
@@ -4,10 +4,13 @@ extends Button
 @export_group("Sounds")
 @export var on_click_sound: AudioStream
 @export var on_hover_sound: AudioStream
-
+@export var bus: String
+@export var volume_db: float
 
 func play_sound(sound: AudioStream):
 	var audio_player = OneShotAudio.new(sound)
+	audio_player.bus = bus
+	audio_player.volume_db = volume_db
 	GlobalSounds.add_child(audio_player)
 
 
